297feb7c0e7d78b2ce33eeee36382b2f663a2130
BabashevaE <118519551+BabashevaE@users.noreply.github.com>
2022-12-12 09:41:47 +0300

    Add files via upload

---
+++ Задача41.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Введите M чисел, через пробел или запятую:");
+string a = Console.ReadLine();
+int[] mass=Array.ConvertAll(a.Split(' ',','),int.Parse);
+Console.WriteLine("Чисел больше нуля: "+CountBolsheNull(mass));
+int CountBolsheNull(int[] array)
+{
+    int i=0,z=0;
+    for (i=0;i<mass.Length;i++)
+    {
+        if (mass[i]>0)
+        z++;
+    }
+    return z;
+}
